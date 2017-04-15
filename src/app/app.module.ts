6aea0312ce3d4ae3776c732fba505373ea085d06
jaavargasar <jaavargasar@gmail.com>
2017-04-15 04:21:05 -0500

    authentication with tokens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommentsComponent } from './comments/comments.component';
 import {CommentsService} from "./comments.service";
+import {RequestService} from "./request.service";
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentsComponent
+    CommentsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [CommentsService],
+  providers: [CommentsService, RequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {CommentsService} from "../comments.service";
+import {RequestService} from "../request.service";
 
-var testio;
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import {Observable} from "rxjs/Observable";
+import { Comment } from './comments';
+
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
@@ -10,15 +16,99 @@ var testio;
 
 
 export class CommentsComponent implements OnInit {
+  comments: Comment[];
+  title = 'List Comments';
 
-  constructor( private commentsService: CommentsService ) {
 
+  constructor( private commentsService: RequestService) {
+      this.commentsService;
+  }
+  //URL = 'https://jsonplaceholder.typicode.com/posts';
+
+
+  getComments(): void {
+    this.commentsService.getUsers('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(
+        comments => {
+          this.comments = comments;
+          console.log('this.users=' + JSON.stringify(this.comments));
+          console.log('this.users.length=' + this.comments.length);
+          console.log('this.users[0].email=' + JSON.stringify(this.comments[0]["userEmail"]) );
+        }, //Bind to view
+        err => {
+          // Log errors if any
+          console.log(err);
+        })
   }
 
   ngOnInit( ) {
-    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4"+this.commentsService.search() );
-    testio = this.commentsService.search();
+    this.getComments();
   }
-  //testio = this.commentsService.search();
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Component, OnInit } from '@angular/core';
+ import {RequestService} from "../comments.service";
+
+ import { Injectable } from '@angular/core';
+ import { Http, Response } from '@angular/http';
+ import 'rxjs/add/operator/toPromise';
+ import 'rxjs/add/operator/map';
+ import {Observable} from "rxjs/Observable";
+
+ @Component({
+ selector: 'app-comments',
+ templateUrl: './comments.component.html',
+ styleUrls: ['./comments.component.css']
+ })
+
+
+ export class CommentsComponent implements OnInit {
+
+
+ private _data: Observable<any[]>;
+ constructor( private http: Http ) {
+ this.getAllUser();
+ }
+
+
+ngOnInit( ) {
+
+}
+
+getAllUser() {
+  return this.http.get('http://www.omdbapi.com/?s=frozen')
+    .subscribe(
+      data => this._data = data.json(),
+      err => this.logError(err),
+      () => console.log('User api call')
+    );
+}
+private logError(err: any) {
+  console.log(err)
+
+}
+
+}
+*/
