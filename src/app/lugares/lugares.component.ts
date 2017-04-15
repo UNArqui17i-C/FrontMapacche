d2403cb67c4244b216a666e406cf2b5a38d9203c
Alt <allozanot@unal.edu.co>
2017-04-15 00:57:19 -0500

    se añadieron pines

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -7,7 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LugaresComponent {
-  title: string = ' ';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+
+    //Posicion Inicial
+
+    latitud: number = 4.638194;
+    longitud: number = -74.084046;
+    zoom: number = 12;
+
+    markers: marker []= [
+          {
+             nombre: 'UNAL',
+             latitud:  4.638194,
+             longitud:  -74.084046,
+             arrastrable: true
+          },
+          {
+             nombre: 'park way',
+             latitud:  4.629177,
+             longitud:  -74.075212,
+             arrastrable: true
+          },
+          {
+             nombre: 'Monserrate',
+             latitud:  4.605673,
+             longitud:  -74.055525,
+             arrastrable: true
+          },
+
+];
+
+    constructor () {
+    }
+
 }
+
+  interface marker {
+    nombre?: string;
+    latitud:number;
+    longitud:number;
+    arrastrable:boolean;
+}
+
+
