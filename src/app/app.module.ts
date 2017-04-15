7d3f9a10d245ff38a262a89a4048af417fe9e233
nordonezc <nordonezc@unal.edu.co>
2017-04-14 23:10:27 -0500

    front
    front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommentsComponent } from './comments/comments.component';
 import {CommentsService} from "./comments.service";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentsComponent
+    CommentsComponent,
+    HomeComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
---
+++ src/app/lugares/lugares.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lugares',
+  templateUrl: './lugares.component.html',
+  styleUrls: ['./lugares.component.css']
+})
+
+export class LugaresComponent {
+  title: string = ' ';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+}
